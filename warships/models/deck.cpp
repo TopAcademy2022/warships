c51da8c42ad27f12106076b2220c22a7a3ac03cb
Misha <shkalaberda-m@yandex.ru>
2023-04-14 20:57:37 +0300

    Added an implementation for the Deck class (deck.cpp). Closed #18

--- warships/models/deck.cpp
+++ warships/models/deck.cpp
@@ -2,20 +2,22 @@
 
 Deck::Deck(unsigned int positionX, unsigned int positionY, bool isAlive)
 {
-
+	this->_positionX = positionX;
+	this->_positionY = positionY;
+	this->_isAlive = isAlive;
 }
 
 unsigned int Deck::GetPositionX()
 {
-	
+	return this->_positionX;
 }
 
 unsigned int Deck::GetPositionY()
 {
-	
+	return this->_positionY;
 }
 
 bool Deck::GetIsAlive()
 {
-	
+	return this->_isAlive;
 }
