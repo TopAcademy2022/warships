a9ae7333986bb688f0b6f2a8aa8c602c00832e43
NeBuratino <Vrn.Liv@gmail.com>
2023-04-14 21:18:06 +0300

    Added an implementation for the Game class (game.cpp). Closed #20

--- warships/models/game.cpp
+++ warships/models/game.cpp
@@ -2,15 +2,16 @@
 
 Game::Game(std::list<Battlefield*> battlefield, unsigned int lastTime)
 {
-
+	this->_battlefield = battlefield;
+	this->_lastTime = lastTime;
 }
 
 std::list<Battlefield*> Game::GetBattlefield()
 {
-	
+	return this->_battlefield;
 }
 
 unsigned int Game::GetLastTime()
 {
-	
+	return this->_lastTime;
 }
