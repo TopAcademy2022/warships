f4a8e38f641aa4fd5e81a8b88490ec6c6a69b50a
IgorGubin23 <rpfmayak@yandex.ru>
2023-04-14 20:43:19 +0300

    Added an implementation for the Battlefield class (battlefield.cpp). Closed #21

--- warships/models/battlefield.cpp
+++ warships/models/battlefield.cpp
@@ -2,15 +2,16 @@
 
 Battlefield::Battlefield(std::list<Ship*> ships, unsigned int countShips)
 {
-
+	this->_ships = ships;
+	this->_countShips = countShips;
 }
 
 std::list<Ship*> Battlefield::GetShips()
 {
-	
+	return this->_ships;
 }
 
 unsigned int Battlefield::GetCountShips()
 {
-	
+	return this->_countShips;
 }
