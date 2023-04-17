f46ae8d109aa7c7cee37adf0ded93b19ede08dfe
Mi-stik31 <25-medvedev@mail.ru>
2023-04-17 20:17:01 +0300

    Added new class for Notepad database (notepad-database.h). Closed #35

---
+++ warships/notepad-database.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+#include "models/i-database.h"
+#include <fstream>
+
+class NotepadDatabase : public IDatabase
+{
+private:
+	char* _dataOfShip;
+
+public:
+	bool WriteDataToFile(std::string subdirectoryName, std::string fileName, std::string fileType);
+
+	bool ReadDataFromFile(std::string subdirectoryName, std::string fileName, std::string fileType);
+};
