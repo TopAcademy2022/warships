3d8048bb389f57ba3b89768ff62175a05f3b3f9c
i__s_13 <121046708+GodJunior1@users.noreply.github.com>
2023-04-17 19:49:49 +0300

    Added an implementation for the Ship class (ship.cpp). Closed #19 (#34)

--- warships/models/ship.cpp
+++ warships/models/ship.cpp
@@ -2,14 +2,16 @@
 
 Ship::Ship(std::list<Deck*> decks, unsigned int shipSize)
 {
+	this->_decks = decks;
+	this->_shipSize = shipSize;
 }
 
 std::list<Deck*> Ship::GetDecks()
 {
-	
+	return this->_decks;
 }
 
 unsigned int Ship::GetShipSize()
 {
-	
+	return this->_shipSize;
 }
