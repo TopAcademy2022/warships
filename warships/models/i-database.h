69d30f5b95f3cdef8a0d1e8b72f07c5e05bd77e5
LootGoblin <112968082+LootGoblin7331@users.noreply.github.com>
2023-07-26 20:44:07 +0300

    Add new methods to IDataBase class (i-database.h). Closed #46 (#47)

--- warships/models/i-database.h
+++ warships/models/i-database.h
@@ -1,12 +1,12 @@
 #pragma once
 
-
 #include <string>
+#include "game.h"
 
 __interface IDatabase
 {
 public:
-	bool WriteDataToFile(std::string subdirectoryName, std::string fileName, std::string fileType);
+	bool WriteDataToFile(Game& game, std::string pathToSave, std::string fileName, std::string fileType);
 
-	bool ReadDataFromFile(std::string subdirectoryName, std::string fileName, std::string fileType);
+	bool ReadDataFromFile(Game& game, std::string pathToSave, std::string fileName, std::string fileType);
 };
